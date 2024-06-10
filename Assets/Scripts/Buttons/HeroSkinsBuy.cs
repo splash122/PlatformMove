b7f2ed7ea082472691ecbe6e9e9b654ccdc84992
Yuliya <yrajskaya@wakeapp.com>
2024-06-10 22:07:52 +0400

    Attempt to make hero choose

--- Assets/Scripts/Buttons/HeroSkinsBuy.cs
+++ Assets/Scripts/Buttons/HeroSkinsBuy.cs
@@ -9,11 +9,14 @@ public class HeroSkinsBuy : MonoBehaviour
 
     public void DefaultHeroSkinsBuy(){
         coinsSkinManager.DecrementIfSkinBought(gameManager.firstSkinPrice);
+        PlayerPrefs.SetString("Hero", "Player");
     }
     public void SecondHeroSkinsBuy(){
         coinsSkinManager.DecrementIfSkinBought(gameManager.secondSkinPrice);
+        PlayerPrefs.SetString("Hero", "Boy");
     }
     public void ThirdHeroSkinsBuy(){
         coinsSkinManager.DecrementIfSkinBought(gameManager.thirdSkinPrice);
+        PlayerPrefs.SetString("Hero", "Policeman");
     }
 }
---
+++ Assets/Scripts/Player/ChangeHeroes.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeHeroes : MonoBehaviour
+{
+
+    //public Object girlPrefab;
+    //public Object boyPrefab;
+    // Reference to the Prefab. Drag a Prefab into this field in the Inspector.
+    //public GameObject[] myPrefab;
+    public Transform parentObject;
+    public Transform[] allchildren;
+
+    // This script will simply instantiate the Prefab when the game starts.
+    void Start() {
+        string hero = PlayerPrefs.GetString("Hero", "Player");
+        print("Hero");
+        print(hero);
+        allchildren = new Transform[parentObject.transform.childCount];
+        for (int i = 0; i < allchildren.Length; i++) {
+            allchildren[i] = parentObject.transform.GetChild(i);
+            if (allchildren[i].name == hero) {
+            }
+            else {
+                allchildren[i].gameObject.SetActive(false);
+            }
+        }
+
+    }
+    void Update(){
+
+    }
+        // Instantiate at position (0, 0, 0) and zero rotation.
+        //Instantiate(myPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+
+}
