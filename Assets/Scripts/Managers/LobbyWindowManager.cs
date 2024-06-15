3f07e9c48dca25b7afd78cd806716c594a413649
Yuliya <yrajskaya@wakeapp.com>
2024-06-15 20:05:21 +0400

    Adding checkMarks for levels

--- Assets/Scripts/Managers/LobbyWindowManager.cs
+++ Assets/Scripts/Managers/LobbyWindowManager.cs
@@ -4,6 +4,7 @@ using UnityEngine.UI;
 public class LobbyWindowManager : MonoBehaviour
 {
     public GameObject checkLvl1;
+    public GameObject checkLvl2;
     public Button buttonLvl2;
 
     // Start is called before the first frame update
@@ -13,11 +14,17 @@ public class LobbyWindowManager : MonoBehaviour
         buttonLvl2.interactable = false;
         checkLvl1 = GameObject.FindWithTag("Check1Lvl");
         checkLvl1.SetActive(false);
+        checkLvl2 = GameObject.FindWithTag("Check2Lvl");
+        checkLvl2.SetActive(false);
         int levelPassed = PlayerPrefs.GetInt("LevelPassedNumber",0);
         print("Number of level passed");
         print(levelPassed);
         if(levelPassed == 1){
             checkLvl1.SetActive(true);
+            buttonLvl2.interactable = true;
+        }
+        if(levelPassed == 2){
+            checkLvl2.SetActive(true);
         }
     }
 }
